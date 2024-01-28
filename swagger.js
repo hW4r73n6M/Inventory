67c71261d97ec5502bdb386eddd33a17770b81c5
hW4r73n6M <gui20003@byui.edu>
2024-01-27 23:05:51 -0500

    adding suppliers endpint

--- swagger.js
+++ swagger.js
@@ -4,13 +4,14 @@ const swaggerAutogen = require("swagger-autogen")();
 const doc = {
     info: {
         title: "Inventory API",
-        description: "Inventory Application Programming Interface"
+        description: "Inventory Application Programming Interface",
+        contact: {name: "Herick Guillén", url: "hguillen.com", email: "gui20003@byui.edu"},
     },
     host: "localhost:".concat(environment.HTTP_PORT),
     schemes: ["http"],
 };
 
 const outputFile = './swagger.json';
-const endpointsFiles = ["./src/routes/inventories.route.js"];
+const endpointsFiles = ["./src/routes/inventories.route.js", "./src/routes/suppliers.route.js"];
 
 swaggerAutogen(outputFile, endpointsFiles, doc);
