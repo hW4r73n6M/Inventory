0b2aa9feb9d67e6ce3a18b2088c724f2cce180ba
hW4r73n6M <gui20003@byui.edu>
2024-01-27 22:13:34 -0500

    path update

--- src/routes/swagger.js
+++ src/routes/swagger.js
@@ -2,7 +2,10 @@ const router = require("express").Router();
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("../../swagger.json");
 
-router.use("/api-docs", swaggerUi.serve);
-router.get("/api-docs", swaggerUi.setup(swaggerDocument));
+router.use("/inventory/api-docs", swaggerUi.serve);
+router.get("/inventory/api-docs", swaggerUi.setup(swaggerDocument));
+
+router.use("/supplier/api-docs", swaggerUi.serve);
+router.get("/supplier/api-docs", swaggerUi.setup(swaggerDocument));
 
 module.exports = router;
