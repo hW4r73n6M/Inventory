0f80626d17b172af294d716b5c16c3a375daa592
hW4r73n6M <gui20003@byui.edu>
2024-01-27 22:28:36 -0500

    comment suppliers path

--- src/routes/swagger.js
+++ src/routes/swagger.js
@@ -5,7 +5,7 @@ const swaggerDocument = require("../../swagger.json");
 router.use("/inventory/api-docs", swaggerUi.serve);
 router.get("/inventory/api-docs", swaggerUi.setup(swaggerDocument));
 
-router.use("/supplier/api-docs", swaggerUi.serve);
-router.get("/supplier/api-docs", swaggerUi.setup(swaggerDocument));
+// router.use("/supplier/api-docs", swaggerUi.serve);
+// router.get("/supplier/api-docs", swaggerUi.setup(swaggerDocument));
 
 module.exports = router;
