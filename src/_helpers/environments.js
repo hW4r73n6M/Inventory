65af88a5dd9a5aa1ff6d85e1a53c949651bd930f
hW4r73n6M <gui20003@byui.edu>
2024-02-05 14:43:34 -0500

    adding .env

--- src/_helpers/environments.js
+++ src/_helpers/environments.js
@@ -1,7 +1,5 @@
-const MONGO_USER = 'hguillen';
-const MONGO_PASSWORD = 'aWDRzW4u4qfmxXb2';
-const MONGO_SERVER = 'cluster0.q5nojgs.mongodb.net';
-const MONGO_DB = 'cse341';
+const dotenv = require('dotenv');
+dotenv.config();  
 
 exports.MongoClient = require('mongodb').MongoClient;
 exports.ObjectId = require('mongodb').ObjectId;
@@ -9,7 +7,3 @@ exports.mongoServer = `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_SER
 exports.mongoDB = `${MONGO_DB}`;
 exports.ENV = 'Development';
 exports.HTTP_PORT = '7443';
-
-const GITHUB_CLIENT_ID = 'b51154d7fb9b0719c89b';
-const GITHUB_CLIENT_SECRET = '7273230466bd780d717c2c3141f1dd4a7d8debb4';
-const CALLBACK_URL = 'http://localhost:7443/github/callback';
