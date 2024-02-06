738115254283443825b983ff5e181fabb712d3c3
hW4r73n6M <gui20003@byui.edu>
2024-02-05 21:04:15 -0500

    login-logout

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,9 +4,9 @@ const passport = require('passport');
 
 router.use('/', require('./swagger'));
 
-router.get('/', (req, res) => {
-    res.send('API Inventory & Supplier');
-});
+// router.get('/', (req, res) => {
+//     res.send('API Inventory & Supplier');
+// });
 
 router.use("/inventories", require('./inventories.route'));
 router.use("/suppliers", require('./suppliers.route'));
